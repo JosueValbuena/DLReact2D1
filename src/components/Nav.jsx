b1dfaaabb1a8a225f8e271f2c1d114490dcfb84b
JosueValbuena <joshvlbn@gmail.com>
2023-03-23 16:41:08 -0300

    componente nav agregado

---
+++ src/components/Nav.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div className='nav'>
+        <h1>Logo</h1>
+        <div>
+            <a href="#">Home</a>
+            <a href="#">Contacto</a>
+            <a href="#">Sobre Nosotros</a>
+        </div>
+    </div>
+  )
+}
+
+export default Nav
